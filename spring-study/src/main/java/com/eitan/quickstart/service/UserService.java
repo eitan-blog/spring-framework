2847ac4296af1c07a83ef3f8b0d6b4cd3cf28b5c
eitan <18101612160@163.com>
2021-07-22 09:49:51 +0800

    2021-07-16 BeanFactoryPostProcessor

---
+++ spring-study/src/main/java/com/eitan/quickstart/service/UserService.java
@@ -0,0 +1,10 @@
+package com.eitan.quickstart.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+	public UserService() {
+		System.out.println("initialization success......");
+	}
+}
