60b924bd4cfcf467f40cbbb9051d15514a53b890
eitan <18101612160@163.com>
2021-08-07 13:26:58 +0800

    2021-07-16 Condition

--- spring-study/src/main/java/com/eitan/condition/MyApplicationContext.java
+++ spring-study/src/main/java/com/eitan/condition/MyApplicationContext.java
@@ -1,9 +1,6 @@
 package com.eitan.condition;
 
-import com.eitan.tag.entity.User;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
--- spring-study/src/main/java/com/eitan/condition/condition/ExistBossCondition.java
+++ spring-study/src/main/java/com/eitan/condition/condition/ExistBossCondition.java
@@ -8,6 +8,6 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 public class ExistBossCondition implements Condition {
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-		return context.getBeanFactory().containsBeanDefinition(Boss.class.getName());
+		return context.getBeanFactory().containsBeanDefinition(Boss.class.getSimpleName().toLowerCase());
 	}
 }
